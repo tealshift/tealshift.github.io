a810a5aeafebc7614ff3d17850ef91c759501cf3
TealShift <tealshift@pkfitness.com>
2020-05-29 04:35:24 -0700

    Added comments and source

--- GeoMapping/geomap.js
+++ GeoMapping/geomap.js
@@ -34,18 +34,21 @@ var view = svg.append("g")
 
 view.selectAll("rect")
     .data(color.range().map((d) => {
+        // For each color, get the x position start and end
         d = color.invertExtent(d)
+        // The replace undefined with the start and end of the domain
         if (d[0] == null) d[0] = x.domain()[0]
         if (d[1] == null) d[1] = x.domain()[1]
         return d
     }))
+    // Add a rect for each color and place it
     .enter().append("rect")
         .attr("height", 8)
         .attr("x", d=>x(d[0]))
         .attr("y", 0)
         .attr("width", d => x(d[1]) - x(d[0]))
         .attr("fill", d=>color(d[0]))
-
+// Add a title for the color legend
 view.append("text")
     .attr("class", "caption")
     .attr("x", x.range()[0])
@@ -68,13 +71,11 @@ Promise.all([
     d3.json("Norway.json"),
     d3.json("PopulationDensity.json")
 ]).then(([geoJson, stats]) => {
+    // This function does all the dirty work to get the info we need
     var features = parseGeoFeatures(geoJson, stats)
-
-    const maxPopDensity = d3.max(features, f=>f.properties.popDensity)
-    console.log("Max density: "+maxPopDensity)
-
     console.log(features)
-    //Norwar coords: [60.4720, 8.4689]
+    //Norway coords: [60.4720, 8.4689]
+    // Trial and error scale, offset, and center values
     var scale  = 2400
     var offset = [canvasSize.width/2, canvasSize.height/2]
     var projection = d3.geoAlbers()
@@ -92,17 +93,19 @@ Promise.all([
        .data(features)
        .enter()
        .append("path")
-       .attr("id", d=>d.name)
-       .attr("d", path)
+       .attr("id", d=>d.name) // Useful for referencing
+       .attr("d", path) // Apply the geographic border
        .style("stroke", "white")
        .style("stroke-width", 1)
        .style("fill", d => {
            console.log(`${d.properties.NAME_1}: ${d.properties.popDensity} per km^2`)
+           // Color the county if we have data for it
            if (d.properties.popDensity) {
                return color(d.properties.popDensity)
            }
            return "#444"
        })
+       // Add mouseover outlines for each county
        .on("mousemove", (d)=> {
            d3.select(`#${d.name}`)
             .style("stroke", "teal")
@@ -115,11 +118,6 @@ Promise.all([
 });
 
 // Format the imported data
-function buildData(rawStats) {
-    console.log(`Parsing stats ${rawStats}}`)
-    var stats = []
-}
-
 function parseGeoFeatures(json, stats) {
     // Unwind polygons from features
     var features = json.features
@@ -136,6 +134,7 @@ function parseGeoFeatures(json, stats) {
                 ring.reverse();
             })
         }
+        // Add a name from the raw county name (with spaces removed)
         feature.name = feature.properties.NAME_1.replace(/\s/g, '')
     })
     // Match county names from stats and copy them to each land feature
@@ -152,52 +151,3 @@ function parseGeoFeatures(json, stats) {
     })
     return features
 }
-
-// Add a group to the SVG that holds data content
-// var view = svg.append("g").attr("id", "view")
-
-// var legend = svg.append("g").attr("id", "legend")
-//     .attr("pointer-events", "none")
-//     .attr("transform", `translate(${margin.left+width-legendSize.width},`
-//                             +` ${margin.top+height-legendSize.height-8})`)
-//     .attr("opacity", 0.8)
-// var tooltip = svg.append("g").attr("id", "tooltip")
-//     .attr("visibility", "hidden")
-//     .attr("pointer-events", "none")
-// var dotLabels = undefined
-// var dots = undefined
-
-
-
-// Zoom and Pan
-// var zoom = d3.zoom()
-//     .scaleExtent([1, 10])
-//     .translateExtent([
-//         // X starts at 0 and can go to the right edge of the canvas
-//         // Y starts at 0 and can go to the bottom of the canvas
-//         [0, 0], [canvasSize.width, canvasSize.height]
-//     ])
-//     .on("zoom", zoomed)
-// svg.call(zoom)
-
-// function zoomed() {
-//     var transform = d3.event.transform
-//     var scale = transform.k
-//     // Apply the zoom transformation to the graph content
-//     view.attr("transform", transform)
-//     // Rescaling the axes allows them to follow the zoom level/position
-//     xAxisG.call(xAxis.scale(transform.rescaleX(xScale)))
-//     yAxisG.call(yAxis.scale(transform.rescaleY(yScale)))
-//     // Inverse scale strokes so they don't get thicker as zoom increases
-//     dots.style("stroke-width", strokeWidth / scale)
-//     dotLabels.style("font-size", labelSize/scale+"px")
-//     view.selectAll(".grid").attr("stroke-width", strokeWidth/scale)
-//     updateLegend(scale)
-//     moveTooltip()
-// }
-// function resetZoom() {
-//     svg.transition()
-//         .duration(500)
-//         .call(zoom.transform, d3.zoomIdentity);
-// }
-
